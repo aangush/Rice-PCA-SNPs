b00705dce78ed2bea4470082efe84758a61cef20
Aidan Angus-Henry <aangushenry@ucdavis.edu>
2022-06-24 17:49:55 -0700

    basic PCA, MDS plots finished in initial R file, fixed formatting in rmd

--- scripts/rice_SNP_initial.R
+++ scripts/rice_SNP_initial.R
@@ -21,3 +21,84 @@ data <- data.geno[,sample(2:ncol(data.geno), 10000)] # Subset the data
 id <- select(data.geno, ID) # Extract the ID column
 data.geno.10k <- data.frame(id, data) # Combine the ID column and 10k SNP subset
 dim(data.geno.10k)
+
+# Fill missing data in with average genotype
+geno.numeric.fill <-
+  apply(geno.numeric, 2, function(x) {
+    x[is.na(x)] <- mean(x, na.rm=T)
+    x})
+
+# Compute the PCs and return the first 10
+geno.pca <- prcomp(geno.numeric.fill, 
+                   rank.=10)
+
+# Calculate the proportion of variance covered by each PC (for fun ;))
+pcvar <- geno.pca$sdev^2 # square std dev to get variance
+pcvar.pct <- tibble(pctvar=pcvar/sum(pcvar) * 100,
+                    PC=1:length(pcvar))
+
+# Plot the proportion of variance covered by each PC
+pcvar.pct[1:10,] %>% 
+  ggplot(mapping = aes(x=PC, y=pctvar)) +
+  geom_col(fill="darkblue")
+
+# Create a tibble with PCs and IDs
+PCs <- as_tibble(geno.pca$x) %>% # The calculated PCs
+  mutate(ID=data.geno.10k$ID) %>%
+  select(ID, everything())
+
+# Plot the PCs
+PCs %>% 
+  ggplot(mapping = aes(x=PC1, y=PC2)) +
+  labs(title="PC1 vs PC2") +
+  geom_point(color="darkblue")
+
+PCs %>% 
+  ggplot(mapping = aes(x=PC3, y=PC2)) +
+  labs(title="PC3 vs PC2") +
+  geom_point(color="darkblue")
+
+# Create an MDS plot
+# calculate the Euclidian distance between each rice variety
+genDist <- as.matrix(dist(geno.numeric))
+
+# perform multi-dimensional scaling
+geno.mds <- as_tibble(cmdscale(genDist))
+
+# Add the variety ID back into this
+geno.mds$ID <- data.geno.10k$ID 
+head(geno.mds) #now we have 2 dimensions + the ID
+
+# Plot V1 vs V2 
+geno.mds %>% 
+  ggplot(mapping = aes(x=V1, y=V2)) +
+  labs(title = "MDS Plot, V1 vs V2") +
+  geom_point(color="darkblue") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
